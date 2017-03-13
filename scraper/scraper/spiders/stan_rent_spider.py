bc3acd779d1e3003bdae4ad051220afadcf67713
franraknic <fran.raknic@gmail.com>
2017-03-13 22:45:44 +0100

    added link extraction to stan_rent_spider

--- scraper/scraper/spiders/stan_rent_spider.py
+++ scraper/scraper/spiders/stan_rent_spider.py
@@ -10,3 +10,23 @@ class RentStanSpider(scrapy.Spider):
     allowed_domains = ['njuskalo.hr']
     start_urls = ['http://www.njuskalo.hr/iznajmljivanje-stanova']
     url_base = 'http://www.njuskalo.hr'
+
+    def parse(self, response):
+
+        zup = response.xpath(u'//div[@class = "list-topcats"]//li/a/@href').extract()
+
+        for z in zup:
+            yield scrapy.Request(self.url_base + z, callback=self.oglasi_links)
+
+    def oglasi_links(self, response):
+
+        links_oglasi = response.xpath(u'//ul[@class = "EntityList-items"]/li/article/h3/a/@href').extract()
+
+        for link in links_oglasi:
+            if link[:12] == u'/nekretnine/':
+                yield scrapy.Request(self.url_base + link, callback=self.parse_oglas)
+            else:
+                log.msg('Rejecting link: %s' % link, level=log.INFO)
+
+    def parse_oglas(self, response):
+        pass
