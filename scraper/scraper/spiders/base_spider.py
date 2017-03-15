087bba14070db57150b8a858f1713954911a7fcb
franraknic <fran.raknic@gmail.com>
2017-03-15 22:46:14 +0100

    moved link extraction to base spider

---
+++ scraper/scraper/spiders/base_spider.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy import log
+
+
+class BaseNekretnineSpider(scrapy.Spider):
+
+    name = ''
+    tip = ''
+    allowed_domains = ['']
+    url_base = ''
+    start_urls = []
+
+    def parse(self, response):
+
+        zup = response.xpath(u'//div[@class = "list-topcats"]//li/a/@href').extract()
+
+        for z in zup:
+            yield scrapy.Request(self.url_base + z, callback=self.oglasi_links)
+
+    def oglasi_links(self, response):
+
+        links_oglasi = response.xpath(u'//ul[@class = "EntityList-items"]/li/article/h3/a/@href').extract()
+
+        for link in links_oglasi:
+            if link[:12] == u'/nekretnine/':
+                yield scrapy.Request(self.url_base + link, callback=self.parse_oglas)
+            else:
+                log.msg('Rejecting link: %s' % link, level=log.INFO)
+
+    def parse_oglas(self, response):
+        pass
